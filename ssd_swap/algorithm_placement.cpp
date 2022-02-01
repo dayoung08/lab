fbb393c55856068edf78111153d6b997e77b7a64
dayoung08 <79209189+dayoung08@users.noreply.github.com>
2022-02-01 15:51:17 +0900

    Add files via upload

--- ssd_swap/algorithm_placement.cpp
+++ ssd_swap/algorithm_placement.cpp
@@ -37,7 +37,7 @@ int placement_myAlgorithm(SSD* _SSD_list, VIDEO_SEGMENT* _VIDEO_SEGMENT_list, in
 
 				double ADWD_placement = _SSD_list[ssd_temp].ADWD + (_VIDEO_SEGMENT_list[video_index].size / (_SSD_list[ssd_temp].storage_capacity * _SSD_list[ssd_temp].DWPD));
 				double remained_bandwidth = (_SSD_list[ssd_temp].maximum_bandwidth - (_SSD_list[ssd_temp].bandwidth_usage + _VIDEO_SEGMENT_list[video_index].requested_bandwidth));
-				double remained_write_MB = (_SSD_list[ssd_temp].storage_capacity * _SSD_list[ssd_temp].DWPD) - ((_SSD_list[ssd_temp].total_write_MB + _VIDEO_SEGMENT_list[video_index].size) / _SSD_list[ssd_temp].running_days);
+				double remained_write_MB = ((_SSD_list[ssd_temp].storage_capacity * _SSD_list[ssd_temp].DWPD) * _SSD_list[ssd_temp].running_days) - (_SSD_list[ssd_temp].total_write_MB + _VIDEO_SEGMENT_list[video_index].size);
 
 				double slope = remained_bandwidth / remained_write_MB; //  남은 수명 대비 남은 밴드윗
 				target_ssd_list_with_ratio_sort.insert(make_pair(slope, ssd_temp));
