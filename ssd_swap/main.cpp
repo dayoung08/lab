1f46bd90966c429b8981421d671c9f1c4c7aee84
dayoung08 <79209189+dayoung08@users.noreply.github.com>
2022-03-28 06:20:19 +0900

    Add files via upload

--- ssd_swap/main.cpp
+++ ssd_swap/main.cpp
@@ -12,7 +12,7 @@ int migration_method = 7; // 8~11로 바꾸면 비교스킴
 int num_of_SSDs = 30; // 10, 20, (30), 40, 50
 int num_of_videos = 2000000;// 50만, 100만, (150만), 200만, 250만
 int num_of_new_videos = 0; // 10000, 20000, (30000), 40000, 50000 에서 나누기 NUM_OF_TIMEs
-double num_of_request_per_sec = 28000; //8000
+double num_of_request_per_sec = 20000; //8000
 
 vector<double> result1;
 vector<double> result2;
